f1b3675d7f42dc4345b66e561660584b854796e9
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-05-24 23:37:40 +0200

    add dummy header & footer

---
+++ shop/src/footer.php
@@ -0,0 +1,19 @@
+<body>
+    <footer id="main_footer">
+        <div id="footer_container">
+
+            <div id="footer_left_column">
+                Left Side Menu
+            </div>
+
+            <div id="footer_middle_column">
+                <p>&copy; <?php echo date("Y"); ?></p>
+            </div>
+
+            <div id="footer_right_column">
+                Right Side Menu
+            </div>
+
+        </div>
+    </footer>
+</body>
