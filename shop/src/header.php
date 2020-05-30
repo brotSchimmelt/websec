5bb12805c40001902e7db6dc9f105a2276faec07
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-05-30 14:21:55 +0200

    add new navbar

--- shop/src/header.php
+++ shop/src/header.php
@@ -8,6 +8,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="resources/css/bootstrap.css">
 
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto:wght@300&display=swap" rel="stylesheet">
+
     <!-- Custom CSS to overwrite Bootstrap.css -->
     <link rel="stylesheet" href="resources/css/custom.css">
 
@@ -16,47 +19,80 @@
 
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-            <ul class="navbar-nav mr-auto">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark" id="defaultNav">
+        <div class="navbar-collapse collapse">
+            <a class="mx-auto navbar-brand" href="#"><img class="mb-4" src="resources/img/wwu-cysec.png" width="140" height="75"></a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsedNavBar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse">
+            <ul class="navbar-nav mx-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="main.php">Home</a>
+                    <a href="main.php" class="nav-link">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="overview.php">Products</a>
+                    <a href="product.php" class="nav-link">Products</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.php">Contact</a>
+                    <a href="contact.php" class="nav-link">Contact</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="cart.php">Shopping Cart</a>
+                    <a href="#" class="nav-link">Find Your Friends</a>
                 </li>
             </ul>
         </div>
-        <div class="mx-auto order-0">
-            <a class="navbar-brand mx-auto" href="#">WebSec Shop</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-            <ul class="navbar-nav ml-auto">
+        <div class="navbar-collapse collapse">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a href="cart.php" class="nav-link">Shopping Cart</a>
+                </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Account
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <div class="dropdown-menu" aria-labelledby="#navbarDropdown">
                         <a class="dropdown-item" href="scoreboard.php">Scoreboard</a>
                         <a class="dropdown-item" href="reset_db.php">Reset Database</a>
                         <a class="dropdown-item" href="help.php">Help</a>
+                        <a class="dropdown-item" href="logout.php">Logout</a>
                     </div>
                 </li>
+            </ul>
+        </div>
+        <div class="collapse" id="collapsedNavBar">
+            <div>
+                <a class="navbar-brand" href="main.php">WebSec Shop</a>
+            </div>
+            <ul class="navbar-nav mx-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Logout</a>
+                    <a href="product.php" class="nav-link">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Find Your Friends</a>
+                </li>
+                <li class="nav-item">
+                    <a href="contact.php" class="nav-link">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a href="cart.php" class="nav-link">Shopping Cart</a>
+                </li>
+                <li class="nav-item">
+                    <a href="scoreboard.php" class="nav-link">Scoreboard</a>
+                </li>
+                <li class="nav-item">
+                    <a href="reset_db.php" class="nav-link">Reset Database</a>
+                </li>
+                <li class="nav-item">
+                    <a href="help.php" class="nav-link">Help</a>
+                </li>
+                <li class="nav-item">
+                    <a href="logout.php" class="nav-link">Logout</a>
                 </li>
             </ul>
         </div>
     </nav>
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
