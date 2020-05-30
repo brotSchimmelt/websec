801971a9cdf521b2477a83fcb0af15406ab3e5cb
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-05-30 18:38:03 +0200

    add close button in header of modal

--- shop/src/footer.php
+++ shop/src/footer.php
@@ -25,6 +25,9 @@
                     <h4 class="modal-title" id="new-id">
                         Help
                     </h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 <div class="modal-body">
                     <h5>Cross-Site Scripting</h5>
@@ -81,9 +84,5 @@
                 </div>
             </div>
         </div>
-
-
-
     </div>
-
 </body>
