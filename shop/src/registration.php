c0e3ec53509abdb2f8b30d42aa81aacc3d5bdaa8
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-05 18:22:10 +0200

    add todo for registration

--- shop/src/registration.php
+++ shop/src/registration.php
@@ -1,3 +1,22 @@
 <?php
 require("$_SERVER[DOCUMENT_ROOT]/../config/db_login_config.php"); // load DB credentials
 require("$_SERVER[DOCUMENT_ROOT]/../src/functions.php"); // load extra functions
+
+
+// TODO: open db connection
+// TODO: check db connection
+// TODO: close db connection (?)
+
+// TODO: validate username
+
+// TODO: validate mail
+
+// TODO: validate password
+
+// TODO: hash password
+
+// TODO: add user account to db
+
+// TODO: redirect to login page
+
+// TODO: error handling in case of something breaking the register process
