9440ab51b95c4891d4a1b464e1a8c33bc9e3374b
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-07 16:41:47 +0200

    add pdo configuration

--- shop/config/db_user_config.php
+++ shop/config/db_user_config.php
@@ -1,7 +1,21 @@
 <?php
 
 // Dummy credentials from the docker .env file
-define("DB_HOST", "db_shop");
-define("DB_NAME", "shop");
-define("DB_USER", "shop");
-define("DB_PASS", "shop");
+$dbHost = 'db_shop';
+$dbName   = 'shop';
+$dbUser = 'shop';
+$dbPass = 'shop';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+    $pdoLogin = new PDO($dsn, $dbUser, $dbPass, $options);
+} catch (Exception $e) {
+    // TODO: add verbose error message 
+    echo "upps";
+}
