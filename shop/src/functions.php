165f0532d1b02aef382aa1087b53a1819928c8e6
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-07 17:37:52 +0200

    delete obsolete function

--- shop/src/functions.php
+++ shop/src/functions.php
@@ -1,14 +1,1 @@
 <?php
-
-// checks if one value in a given array is empty
-function check_for_empty_values($value_array)
-{
-    $is_empty = False;
-
-    foreach ($value_array as $value) {
-        if (empty($value)) {
-            $is_empty = True;
-        }
-    }
-    return $is_empty;
-}
