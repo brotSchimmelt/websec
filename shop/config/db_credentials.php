1f9818aafb72afa69eb75e658506cfdeefd9c56d
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-08 10:28:20 +0200

    add db credentials to new file

--- shop/config/db_credentials.php
+++ shop/config/db_credentials.php
@@ -2,8 +2,8 @@
 
 // Dummy credentials from the docker example.env file
 // TODO: change credentials
-define($DB_HOST, 'db_shop');
-define($DB_NAME, 'shop');
-define($DB_USER, 'shop');
-define($DB_PWD, 'shop');
-define($CHARSET, 'utf8mb4');
+define("DB_HOST", "db_shop");
+define("DB_NAME", "shop");
+define("DB_USER", "shop");
+define("DB_PWD", "shop");
+define("CHARSET", "utf8mb4");
