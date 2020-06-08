a710995b2266c5009c4b6435f05a676de84eaf88
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-08 12:18:40 +0200

    add option for login button

--- shop/public/action_handler.php
+++ shop/public/action_handler.php
@@ -2,6 +2,8 @@
 
 if (isset($_POST['register-submit'])) {
     require("$_SERVER[DOCUMENT_ROOT]/../src/registration_script.php");
+} else if (isset($_POST['login-submit'])) {
+    require("$_SERVER[DOCUMENT_ROOT]/../src/login_script.php");
 } else {
     // TODO: add redirect to login page!
     echo "<b>Nope.</b>";
