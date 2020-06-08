6f3d0e1b966d31e21674ec5a8f7178c5567b3eb4
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-08 10:04:54 +0200

    add helper functions for registration

--- shop/src/functions.php
+++ shop/src/functions.php
@@ -1,1 +1,41 @@
 <?php
+
+// check if username is in scope and 2 <= len <= 64
+function validate_username($username)
+{
+    if (!preg_match("/^[A-Za-z0-9]*$/", $username)) {
+        return false;
+    } else if ((strlen($username) > 64) || (strlen($username) < 2)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validate_mail($mailAddress)
+{
+    if ((strstr($mailAddress, "@") == "@uni-muenster.de") || ((strstr($mailAddress, "@") == "@wi.uni-muenster.de"))) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validate_pwd($pwd)
+{
+    if (!$pwd || strlen($pwd) < 8) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function hash_user_pwd($pwd)
+{
+    $hash = password_hash($pwd, PASSWORD_DEFAULT, ['cost' => 13]);
+    // check if the hash was successfully created
+    if (!$hash) {
+        throw new Exception("Hash creation failed: Error Code 42. Please post this error in Learnweb.");
+    }
+    return $hash;
+}
