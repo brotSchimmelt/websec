355346672497aa792d31ebe13fc7beb6304c6a1c
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-08 10:05:12 +0200

    add registration script functionality

--- shop/src/registration_script.php
+++ shop/src/registration_script.php
@@ -1,25 +1,69 @@
 <?php
-require("$_SERVER[DOCUMENT_ROOT]/../config/db_user_config.php"); // load DB credentials
-require("$_SERVER[DOCUMENT_ROOT]/../src/functions.php"); // load extra functions
+// load DB connection
+require("$_SERVER[DOCUMENT_ROOT]/../config/db_user_config.php");
+// load extra functions
+require("$_SERVER[DOCUMENT_ROOT]/../src/functions.php");
 
-// TODO: check if user came from the registration page
+// paths
+$registerPage = "registration.php";
+$loginPage = "index.php";
 
-// TODO: open db connection
-// TODO: check db connection
-// TODO: close db connection (?)
+// check if username AND mail are okay
+if (!validate_username($_POST['username']) && !validate_mail($_POST['mail'])) {
+    header("Location: " . $registerPage . "?error=invalidNameAndMail");
+    exit();
+}
+// validate the username alone
+else if (!validate_username($_POST['username'])) {
+    $oldInput = "&mail=" . $_POST['mail'];
+    header("Location: " . $registerPage . "?error=invalidUsername" . $oldInput);
+    exit();
+}
+// validate the mail adress alone
+else if (!validate_mail($_POST['mail'])) {
+    $oldInput = "&username=" . $_POST['username'];
+    header("Location: " . $registerPage . "?error=invalidMail" . $oldInput);
+    exit();
+}
+// validate the password
+else if (!validate_pwd($_POST['password'])) {
+    $oldInput = "&username=" . $_POST['username'] . "&mail=" . $_POST['mail'];
+    header("Location: " . $registerPage . "?error=invalidPassword" . $oldInput);
+}
+// check if the passwords match
+else if ($_POST['password'] !== $_POST['confirmPassword']) {
+    $oldInput = "&username=" . $_POST['username'] . "&mail=" . $_POST['mail'];
+    header("Location: " . $registerPage . "?error=passwordMismatch" . $oldInput);
+    exit();
+} else {
+    // check if username already exits in the db
+    try {
+        $duplicateQuery = $pdoLogin->prepare("SELECT 1 FROM users WHERE user_name = ?");
+        $duplicateQuery->execute([$_POST['username']]);
+    } catch (Exception $e) {
+        header("Location: " . $registerPage . "?error=sqlError");
+        exit();
+    }
+    $nameExists = $duplicateQuery->fetchColumn();
+    if ($nameExists) {
+        $oldInput = "&mail=" . $_POST['mail'];
+        header("Location: " . $registerPage . "?error=nameTaken" . $oldInput);
+        exit();
+    }
+    // add user to the db
+    else {
+        $pwdHash = hash_user_pwd($_POST['password']);
 
-// TODO: validate username
-
-// TODO: validate mail
-
-// TODO: validate password
-
-// TODO: hash password
-
-// TODO: add user account to db
-
-// TODO: redirect to login page
-
-// TODO: error handling in case of something breaking the register process
-
-echo "I'm in registration_script!";
+        try {
+            $insertUser = "INSERT INTO users (user_name, user_mail, user_pwd_hash) VALUE (?, ?, ?)";
+            $pdoLogin->prepare($insertUser)->execute([$_POST['username'], $_POST['mail'], $pwdHash]);
+        } catch (Exception $e) {
+            header("Location: " . $registerPage . "?error=sqlError");
+            exit();
+        }
+        // redirect back to login page
+        $loginPage = "index.php";
+        header("location: " . $loginPage . "?signup=success");
+        exit();
+    }
+}
