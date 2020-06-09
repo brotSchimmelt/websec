eb4e156b002a03231c2417bbf768cf0f9d5c9a62
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-09 17:46:21 +0200

    add dashbaord page

---
+++ shop/public/dashboard.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="resources/css/bootstrap.css">
+
+    <title>WebSec |Dashboard</title>
+
+</head>
+
+<body>
+
+</body>
