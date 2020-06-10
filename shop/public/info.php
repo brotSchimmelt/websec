7e80ba67af465ef4e04367433200740f5bc8f14a
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-10 15:03:12 +0200

    update info page

--- shop/public/info.php
+++ shop/public/info.php
@@ -1,2 +1,10 @@
 <?php
-phpinfo();
+// TODO: Add check if admin or not
+// best practice would be a separate function
+$value = true;
+
+if ($value) {
+    phpinfo();
+} else {
+    header("location: index.php");
+}
