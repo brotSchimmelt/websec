26c11bd0a998fb761c9b051a40d72da2a6c3e638
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-10 15:02:57 +0200

    add simple dashboard layout

---
+++ shop/src/sidebar.php
@@ -0,0 +1,54 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="sidebar-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">
+                    <span data-feather="home"></span>
+                    Dashboard <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span data-feather="users"></span>
+                    User Management
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span data-feather="file"></span>
+                    Results
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span data-feather="bar-chart-2"></span>
+                    Export Data
+                </a>
+            </li>
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Technical Settings</span>
+        </h6>
+        <ul class="nav flex-column mb-2">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span data-feather="settings"></span>
+                    Shop Settings
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://localhost:1234">
+                    <span data-feather="settings"></span>
+                    phpMyAdmin
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="info.php">
+                    <span data-feather="settings"></span>
+                    php info
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
