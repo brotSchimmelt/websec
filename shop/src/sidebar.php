7889119600eab2d866a291d2d5e1c8a719ffb1f7
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-10 19:09:24 +0200

    add active link query

--- shop/src/sidebar.php
+++ shop/src/sidebar.php
@@ -2,27 +2,33 @@
     <div class="sidebar-sticky pt-3">
         <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <a class="nav-link <?php echo ($here == "dashboard") ? "active" : ""; ?>" href="dashboard.php">
                     <span data-feather="home"></span>
                     Dashboard <span class="sr-only">(current)</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link <?php echo ($here == "user_management") ? "active" : ""; ?>" href="user_management.php">
                     <span data-feather="users"></span>
                     User Management
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link <?php echo ($here == "results") ? "active" : ""; ?>" href="#">
                     <span data-feather="file"></span>
                     Results
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link <?php echo ($here == "export_data") ? "active" : ""; ?>" href="#">
                     <span data-feather="bar-chart-2"></span>
                     Export Data
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?php echo ($here == "feedback") ? "active" : ""; ?>" href="#">
+                    <span data-feather="file"></span>
+                    Feedback
                 </a>
             </li>
         </ul>
@@ -32,7 +38,7 @@
         </h6>
         <ul class="nav flex-column mb-2">
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link <?php echo ($here == "settings") ? "active" : ""; ?>" href="#">
                     <span data-feather="settings"></span>
                     Shop Settings
                 </a>
@@ -46,7 +52,13 @@
             <li class="nav-item">
                 <a class="nav-link" href="info.php">
                     <span data-feather="settings"></span>
-                    php info
+                    info.php
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?php echo ($here == "logs") ? "active" : ""; ?>" href="#">
+                    <span data-feather="settings"></span>
+                    Log Files
                 </a>
             </li>
         </ul>
