b079bae2cf087ef4061e7005b899549825569084
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-12 23:32:24 +0200

    change send btn id

--- shop/public/password_forgotten.php
+++ shop/public/password_forgotten.php
@@ -33,7 +33,7 @@ session_start();
             <p>Enter your @uni-muenster.de mail address. If you are already registered, you will receive a mail with further instructions to reset your password.</p>
         </div>
 
-        <a href="index.php" id="login_btn" class="btn btn-lg btn-info btn-block">Send Mail</a>
+        <a href="index.php" id="send_btn" class="btn btn-lg btn-info btn-block">Send Mail</a>
         <a href="index.php" class="btn btn-link">Back to Login Page</a>
 
         <p class="mt-5 mb-3 text-muted">&copy; <?php echo date("Y"); ?></p>
