eb2b373c45634f39a7d7fb366d6f1b5f1ad3bf13
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-12 23:13:54 +0200

    add vendor dir for JS

--- shop/config/config.php
+++ shop/config/config.php
@@ -15,5 +15,11 @@ define("BIN", ROOT .  ".." . DS . "docs" . DS); // Command Line Utility dir (cur
 define("ADMIN_DIR", ROOT . "admin" . DS); // Admin dir
 define("SHOP_DIR", ROOT . "shop" . DS); // Shop dir
 
+// Location of assets
+define("CSS", ROOT . "assets" . DS . "css" . DS); // CSS
+define("IMG", ROOT . "assets" . DS . "img" . DS); // Images
+define("JSC", ROOT . "assets" . DS . "js" . DS . "vendor" . DS); // JavaScript
+
+
 define("LOGIN_PAGE", ROOT . "index.php"); // Login page
 define("MAIN_PAGE", ROOT .  "shop" . "main.php"); // Main page
