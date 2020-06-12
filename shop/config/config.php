3dbf65fac69055d7dbb0c8298db6281152e181de
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-12 23:07:55 +0200

    add constants for locations

--- shop/config/config.php
+++ shop/config/config.php
@@ -1,6 +1,19 @@
 <?php
 
-// Web Server root dir
-define("WS_ROOT", $_SERVER['DOCUMENT_ROOT]']);
+define("DS", DIRECTORY_SEPARATOR); // OS independent dir separator
+define("ROOT", $_SERVER['DOCUMENT_ROOT'] . DS); // Web Server root dir
 
-define("LOGIN_PAGE", "");
+// Locations on app level
+define("SRC", ROOT .  ".." . DS . "src" . DS); // Source dir
+define("RES", ROOT .  ".." . DS . "resources" . DS); // Resources dir
+define("DOC", ROOT .  ".." . DS . "docs" . DS); // Documentation dir
+define("LOG", ROOT .  ".." . DS . "docs" . DS); // Log dir
+define("BIN", ROOT .  ".." . DS . "docs" . DS); // Command Line Utility dir (currently empty)
+
+
+// Locations in public/
+define("ADMIN_DIR", ROOT . "admin" . DS); // Admin dir
+define("SHOP_DIR", ROOT . "shop" . DS); // Shop dir
+
+define("LOGIN_PAGE", ROOT . "index.php"); // Login page
+define("MAIN_PAGE", ROOT .  "shop" . "main.php"); // Main page
