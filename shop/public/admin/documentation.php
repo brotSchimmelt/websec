4ca6ef2f671ec45e0545fb54baff78b539f41fe3
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-13 13:03:50 +0200

    add dummy content

--- shop/public/admin/documentation.php
+++ shop/public/admin/documentation.php
@@ -20,6 +20,7 @@ require("$_SERVER[DOCUMENT_ROOT]/../config/config.php");
 </head>
 
 <body>
+    <!-- Simple Navbar -->
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
         <h5 class="my-0 mr-md-auto font-weight-normal">WebSec Documentation</h5>
         <nav class="my-2 my-md-0 mr-md-3">
@@ -29,51 +30,30 @@ require("$_SERVER[DOCUMENT_ROOT]/../config/config.php");
         <a class="btn btn-outline-warning" href="#">Logout</a>
     </div>
 
+    <!-- Headline -->
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <h1 class="display-4">Documentation</h1>
+        <h1 class="display-4">WebSec Documentation</h1>
         <p class="lead">Here is the HTML version of the project documentation.</p>
+        <a href="#"> · Here</a>
+        <a href="#"> · are</a>
+        <a href="#"> · some</a>
+        <a href="#"> · links</a>
     </div>
 
+    <br><br>
+
+    <!-- Content Container -->
     <div class="container">
+
+        <!-- Dashboard Documentation -->
+        <?php include(DOC . "dashboard_doc.html"); ?>
     </div>
 
-    <footer class="pt-4 my-md-5 pt-md-5 border-top">
-        <div class="row">
-            <div class="col-12 col-md">
-                <img class="mb-2" src="/docs/4.5/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
-                <small class="d-block mb-3 text-muted">&copy; 2017-2020</small>
-            </div>
-            <div class="col-6 col-md">
-                <h5>Features</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="#">Cool stuff</a></li>
-                    <li><a class="text-muted" href="#">Random feature</a></li>
-                    <li><a class="text-muted" href="#">Team feature</a></li>
-                    <li><a class="text-muted" href="#">Stuff for developers</a></li>
-                    <li><a class="text-muted" href="#">Another one</a></li>
-                    <li><a class="text-muted" href="#">Last time</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md">
-                <h5>Resources</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="#">Resource</a></li>
-                    <li><a class="text-muted" href="#">Resource name</a></li>
-                    <li><a class="text-muted" href="#">Another resource</a></li>
-                    <li><a class="text-muted" href="#">Final resource</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md">
-                <h5>About</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="#">Team</a></li>
-                    <li><a class="text-muted" href="#">Locations</a></li>
-                    <li><a class="text-muted" href="#">Privacy</a></li>
-                    <li><a class="text-muted" href="#">Terms</a></li>
-                </ul>
-            </div>
-        </div>
-    </footer>
+    <!-- Simple Footer -->
+    <div class="container">
+        <footer class="pt-4 my-md-5 pt-md-5 border-top">
+            <a href="#">Back to the Top</a>
+        </footer>
     </div>
 </body>
 
