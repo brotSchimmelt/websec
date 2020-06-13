f30908213344fb9338225a79ac199f675ae05cb0
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-13 17:34:52 +0200

    add GET var for message handling

--- shop/src/basic_functions.php
+++ shop/src/basic_functions.php
@@ -15,5 +15,5 @@ function log_user_out()
     $_SESSION = array();
     session_destroy();
 
-    header("location: " . "/index.php");
+    header("location: " . "/index.php" . "?login=loggedOut");
 }
