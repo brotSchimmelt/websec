f9b0460fc2d4fb3f8afe5dbcdb7ff6c12282808a
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-13 17:28:23 +0200

    separate functions in new file

--- shop/src/basic_functions.php
+++ shop/src/basic_functions.php
@@ -1,1 +1,16 @@
 <?php
+
+function is_user_logged_in()
+{
+    if (isset($_SESSION['user_login_status']) && $_SESSION['user_login_status'] == 1) {
+        return true;
+    }
+
+    return false;
+}
+
+function log_user_out()
+{
+    $_SESSION = array();
+    session_destroy();
+}
--- shop/src/login_functions.php
+++ shop/src/login_functions.php
@@ -36,19 +36,3 @@ function hash_user_pwd($pwd)
     }
     return $hash;
 }
-
-
-function is_user_logged_in()
-{
-    if (isset($_SESSION['user_login_status']) && $_SESSION['user_login_status'] == 1) {
-        return true;
-    }
-
-    return false;
-}
-
-function log_user_out()
-{
-    $_SESSION = array();
-    session_destroy();
-}
