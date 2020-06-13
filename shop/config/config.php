d3e62507cfab86375264f97af619baad8cb4552d
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-13 11:46:27 +0200

    add new paths

--- shop/config/config.php
+++ shop/config/config.php
@@ -9,16 +9,18 @@ define("RES", ROOT .  ".." . DS . "resources" . DS); // Resources dir
 define("DOC", ROOT .  ".." . DS . "docs" . DS); // Documentation dir
 define("LOG", ROOT .  ".." . DS . "docs" . DS); // Log dir
 define("BIN", ROOT .  ".." . DS . "docs" . DS); // Command Line Utility dir (currently empty)
+define("CON", ROOT .  ".." . DS . "config" . DS); // Log dir
 
-define("CART", ROOT .  ".." . DS . "src" . DS . "cart" . DS); // Cart dir
-define("DASH", ROOT .  ".." . DS . "src" . DS . "dashboard" . DS); // Dashboard dir
-define("PROD", ROOT .  ".." . DS . "src" . DS . "product" . DS); // Product dir
-define("INCL", ROOT .  ".." . DS . "src" . DS . "includes" . DS);  // Includes dir
+// Subfolders in src/
+define("INCL", SRC . "includes" . DS);  // Includes dir
 
-define("HEADER", INCL . "header.php"); // Header location
-define("FOOTER", INCL . "footer.php");  // Footer location
+// Common includes
+define("HEADER_SHOP", INCL . "header.php"); // Header location
+define("FOOTER_SHOP", INCL . "footer.php");  // Footer location
 define("HEADER_DASH", INCL . "dashboard_header.php"); // Dashboard header location
-define("SIDEBAR", INCL . "sidebar.php"); // Dashboard sidebar location
+define("SIDEBAR_DASH", INCL . "sidebar.php"); // Dashboard sidebar location
+define("JS_DASHBOARD", INCL . "dashboard_js.php"); // JavaScript for the admin section
+define("JS_SHOP", INCL . "shop_js.php"); // JavaScript for the shop section
 
 // Relative path to frequent redirect destinations
 define("MAIN_PAGE", "shop" . DS . "main.php");
