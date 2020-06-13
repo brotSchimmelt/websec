a72ad4e2977e01e87d5776e187ef286107168b4d
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-13 17:29:28 +0200

    add comments

--- shop/src/basic_functions.php
+++ shop/src/basic_functions.php
@@ -1,14 +1,15 @@
 <?php
 
+// checks if the user is already logged in
 function is_user_logged_in()
 {
     if (isset($_SESSION['user_login_status']) && $_SESSION['user_login_status'] == 1) {
         return true;
     }
-
     return false;
 }
 
+// logs the user out
 function log_user_out()
 {
     $_SESSION = array();
