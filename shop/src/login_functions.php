43d6f01864dbce2799686e5d988cb55d7aaa1493
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-13 18:17:01 +0200

    add explicit login function

--- shop/src/login_functions.php
+++ shop/src/login_functions.php
@@ -36,3 +36,19 @@ function hash_user_pwd($pwd)
     }
     return $hash;
 }
+
+function do_login($username, $mail, $adminFlag)
+{
+    if (session_status() == PHP_SESSION_NONE) {
+        // Session has not yet started
+        session_start();
+    }
+
+    $_SESSION['user_name'] = $username;
+    $_SESSION['user_mail'] = $mail;
+    $_SESSION['user_login_status'] = 1;
+
+    if ($adminFlag == 1) {
+        $_SESSION['user_is_admin'] = $adminFlag;
+    }
+}
--- shop/src/login_script.php
+++ shop/src/login_script.php
@@ -40,11 +40,8 @@ if (empty($username) || empty($pwd)) {
         $result = $sql->fetch();
         $pwdTest = password_verify($pwd, $result['user_pwd_hash']);
         if ($pwdTest) {
-            // log user in
-            session_start();
-            $_SESSION['user_name'] = $result['user_name'];
-            $_SESSION['user_mail'] = $result['user_mail'];
-            $_SESSION['user_login_status'] = 1;
+
+            do_login($result['user_name'], $result['user_mail'], $result['user_isAdmin']);
 
             header("location: " . MAIN_PAGE . "?login=success");
             exit();
