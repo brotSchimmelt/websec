0336fbbaecbd18d3a808b437291b1b3b9546a7b6
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-14 17:28:14 +0200

    add new formatting

--- shop/public/shop/admin.php
+++ shop/public/shop/admin.php
@@ -36,17 +36,17 @@ if (is_user_logged_in() && is_user_admin()) {
     <body class="text-center">
         <form class="form-signin" action="form_handler.php" method="post">
 
-            <img class="mb-4" src="../assets/img/lotr.png" alt="Stop" width="280" height="150">
+            <img class="mb-4" src="../assets/img/lotr.png" alt="You.. Shall Not.. Pass!" width="280" height="180">
             <h1 class="h3 mb-3 font-weight-normal">Admin Login</h1>
+            <div class="alert alert-danger shadow-sm" role="alert">
+                Please use this login only when you are really an <strong>admin</strong> user!
+            </div>
             <label for="inputName" class="sr-only">Username</label>
             <input type="text" class="form-control" value="admin" placeholder="Username" required autofocus>
             <label for="inputPassword" class="sr-only">Password</label>
             <input type="password" class="form-control" value="password123" placeholder="Password" required>
 
             <!-- <a href="password_forgotten.php" id="forget_btn" class="btn btn-link">Forgot your password?</a> -->
-            <div class="alert alert-danger" role="alert">
-                Only for <strong>admin</strong> users!
-            </div>
             <a class="btn btn-lg btn-primary btn-block" href="admin_redirect.php">Login</a>
             <!-- <a href="registration.php" id="register_btn_link" class="btn btn-lg btn-outline-secondary btn-block">Register</a> -->
             <p class="mt-5 mb-3 text-muted">&copy; <?php echo date("Y"); ?></p>
