bdcc43fc6e10380644c21cbdd07028eb9b2be3d7
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-22 16:57:14 +0200

    rename files

--- shop/config/db_login.php
+++ shop/config/db_login.php
@@ -1,6 +1,13 @@
 <?php
 
-require_once("$_SERVER[DOCUMENT_ROOT]/../config/db_credentials.php");
+// Dummy credentials from the docker example.env file
+// TODO: change credentials to real ones
+define("DB_HOST", "db_shop");
+define("DB_NAME", "shop");
+define("DB_USER", "shop");
+define("DB_PWD", "shop");
+define("CHARSET", "utf8mb4");
+
 
 $dsn =
     "mysql:host=" . DB_HOST .
