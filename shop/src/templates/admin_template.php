b553726a66a0644d088586097c3712a60874b368
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-23 08:16:18 +0200

    create page templates

---
+++ shop/src/templates/admin_template.php
@@ -0,0 +1,45 @@
+<?php
+session_start(); // Needs to be called first on every page
+
+// Load config files
+require_once("$_SERVER[DOCUMENT_ROOT]/../config/config.php");
+
+// Load custom libraries
+require(FUNC_BASE);
+require(FUNC_ADMIN);
+
+// Load error handling and user messages
+require(ERROR_HANDLING);
+
+// Check admin status
+if (!is_user_admin()) {
+    // Redirect to shop main page
+    header("location: " . MAIN_PAGE);
+    exit();
+}
+
+// load POST or GET variables and sanitize input
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/assets/css/bootstrap.css">
+
+    <!-- Custom CSS to overwrite bootstrap.css -->
+    <link rel="stylesheet" href="/assets/css/admin.css">
+
+    <title>[DUMMY TITLE]</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ shop/src/templates/login_template.php
@@ -0,0 +1,55 @@
+<?php
+session_start(); // Needs to be called first on every page
+
+// Load config files
+require_once("$_SERVER[DOCUMENT_ROOT]/../config/config.php");
+require_once(CONF_DB_LOGIN);
+
+// Load custom libraries
+require(FUNC_BASE);
+require(FUNC_LOGIN);
+
+// Load error handling and user messages
+require(ERROR_HANDLING);
+
+// Check login status
+if (is_user_logged_in()) {
+    // Redirect to shop main page
+    header("location: " . MAIN_PAGE);
+    exit();
+} ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+    <!-- Custom CSS to overwrite bootstrap.css -->
+    <link rel="stylesheet" href="assets/css/login.css">
+
+    <title>[DUMMY TITLE]</title>
+</head>
+
+<body>
+    <?php
+    // Load error messages, user notifications etc.
+    require(MESSAGES);
+    ?>
+
+    <!-- HTML content BEGIN -->
+    <!-- HTML content END -->
+
+    <footer></footer>
+
+    <?php
+    // Load JavaScript
+    require_once(JS_BOOTSTRAP); // Default Bootstrap JavaScript
+    ?>
+</body>
+
+</html>
