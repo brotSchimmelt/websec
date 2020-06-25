5d1a1bb68470a62a7c3478e2beecc5fabd74e89e
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-25 23:07:24 +0200

    change login db credentials

--- shop/config/db_login.php
+++ shop/config/db_login.php
@@ -7,21 +7,17 @@ define("DB_NAME", "login");
 define("DB_USER", "root");
 define("DB_PWD", "root");
 define("CHARSET", "utf8mb4");
-
-
-$dsn =
+define(
+    "DSN",
     "mysql:host=" . DB_HOST .
-    ";dbname=" . DB_NAME .
-    ";charset=" . CHARSET;
-
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-    $pdoLogin = new PDO($dsn, DB_USER, DB_PWD, $options);
-} catch (Exception $e) {
-    // TODO: add verbose error message 
-    echo "whoops, the database connection could not be established:-(";
-}
+        ";dbname=" . DB_NAME .
+        ";charset=" . CHARSET
+);
+define(
+    "OPTIONS",
+    [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ]
+);
