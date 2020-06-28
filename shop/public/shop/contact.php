05d05697bd1f9afeea3ce6969f2231feee791b02
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-28 22:10:39 +0200

    add post method

--- shop/public/shop/contact.php
+++ shop/public/shop/contact.php
@@ -19,6 +19,64 @@ if (!is_user_logged_in()) {
 }
 
 // Load POST or GET variables and sanitize input BELOW this comment
+if (isset($_POST['uName']) && isset($_POST['userPost'])) {
+    $referrer = $_SERVER['HTTP_REFERER'];
+    $pos1 = strpos($referrer, "product.php");
+    $pos2 = strpos($referrer, "overview.php");
+    if ($pos1 != false || $pos2 != false) {
+        // request comes from one of the xss sites
+        $userName = $_POST['uName'];
+        $userPost = $_POST['userPost'];
+        if ($userName == $_SESSION['userName']) {
+
+            $sql = "SELECT username FROM crossposts WHERE username";
+
+
+
+
+
+
+            $mysqli = new mysqli("db_websec", "websec", "S0s3zwo9z3hn", "websec");
+            if (mysqli_connect_errno()) {
+                printf("Connect failed: %s\n", mysqli_connect_error());
+                exit();
+            }
+            $query = "SELECT username FROM crossposts WHERE username = '" . $userName . "';";
+            if ($result = $mysqli->query($query)) {
+                if ($result->num_rows < 1) {
+
+                    $timestamp = date("Ymd-His");
+                    echo ($timestamp);
+                    #$mysqli = new mysqli("127.0.0.1", "websec", "S0s3zwo9z3hn", "websec"); if ($mysqli->connect_errno) {
+                    #	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+                    #}
+                    if (!($stmt = $mysqli->prepare("INSERT INTO crossposts (username,post,timestamp,referrer) VALUES (?,?,?,?)"))) {
+                        echo "error (bkwm): (" . $mysqli->errno . ") " . $mysqli->error;
+                    }
+                    if (!$stmt->bind_param("ssss", $userName, $userPost, $timestamp, $referrer)) {
+                        echo "error (yqwh): (" . $stmt->errno . ") " . $stmt->error;
+                    }
+                    if (!$stmt->execute()) {
+                        echo "error (bhwn): (" . $stmt->errno . ") " . $stmt->error;
+                    }
+                    $stmt->close();
+                    echo '<h4>Thank You!</h4>We have received your request and will come back to you very soon.<br>Very soon! Really!<br>One day..<br>or never.';
+                } else {
+                    echo 'you have already posted a request!';
+                }
+            } else {
+                echo 'error (qlem)';
+            }
+            $mysqli->close();
+        } else {
+            // wrong user
+            echo 'error: user mismatch';
+        }
+    } else {
+        // referrer incorrect
+        echo '<h4>Something went wrong!</h4>You tried to contact us but the form is disabled.<br>Sorry, you will have to find another way..<br>Do not manipulate the disabled form!';
+    }
+}
 
 ?>
 <!doctype html>
@@ -57,12 +115,12 @@ if (!is_user_logged_in()) {
     In urgent cases please contact our support team.<br>
     Thanks!<br>
     <br>
-    <form action="thisfile" method="post" id="reviewform">
+    <form action="contact.php" method="post" id="reviewform">
         your name:
-        <input type="text" name="username" value="TestUser" disabled><br>
-        <input type="hidden" name="uname" value="uname">
+        <input type="text" name="username" value="<?= $_SESSION['userName'] ?>" disabled><br>
+        <input type="hidden" name="uName" value="<?= $_SESSION['userName'] ?>">
         your message for us:
-        <input type="text" name="upost" size="30" disabled><br><br>
+        <input type="text" name="userPost" size="30" disabled><br><br>
         <input type="submit" value="Submit" disabled>
     </form>
     <!-- HTML Content END -->
