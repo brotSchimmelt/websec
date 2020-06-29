e4f2157450ef31c1d56bfd30555a31850825c863
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-30 00:05:52 +0200

    add new headline to sidebar

--- shop/src/includes/admin_sidebar.php
+++ shop/src/includes/admin_sidebar.php
@@ -1,5 +1,8 @@
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
     <div class="sidebar-sticky pt-3">
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Student Affairs</span>
+        </h6>
         <ul class="nav flex-column">
             <li class="nav-item">
                 <a class="nav-link <?= ($here == "dashboard") ? "active" : ""; ?>" href="dashboard.php">
