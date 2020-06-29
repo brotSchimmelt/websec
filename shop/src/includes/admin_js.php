bc694cef0dba6f59e284d7f61e6a9171a549c720
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-06-30 01:24:00 +0200

    add search script for user management

--- shop/src/includes/admin_js.php
+++ shop/src/includes/admin_js.php
@@ -1,5 +1,18 @@
-<script src="../assets/js/vendor/jquery-3.5.1.slim.min.js"></script>
+<!-- <script src="../assets/js/vendor/jquery-3.5.1.slim.min.js"></script> -->
+<script src="../assets/js/vendor/jquery-3.5.1.min.js"></script>
 <script src="../assets/js/vendor/bootstrap.bundle.min.js"></script>
 <script src="../assets/js/vendor/feather.min.js"></script>
 <script src="../assets/js/vendor/Chart.min.js"></script>
 <script src="../assets/js/dashboard.js"></script>
+
+<!-- Search Script -->
+<script>
+    $(document).ready(function() {
+        $("#user-search").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#user-table tr").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+    });
+</script>
