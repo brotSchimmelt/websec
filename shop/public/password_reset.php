2488726f7b5968ed16f631b9056957b63b87ee5f
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-08-05 21:00:25 +0200

    update password forgotten form

--- shop/public/password_reset.php
+++ shop/public/password_reset.php
@@ -3,11 +3,11 @@ session_start(); // Needs to be called first on every page
 
 // Load config files
 require_once("$_SERVER[DOCUMENT_ROOT]/../config/config.php");
-// require_once(CONF_DB_LOGIN);
+require_once(CONF_DB_LOGIN);
 
 // Load custom libraries
 require(FUNC_BASE);
-// require(FUNC_LOGIN);
+require(FUNC_LOGIN);
 
 // Load error handling and user messages
 require(ERROR_HANDLING);
@@ -17,7 +17,15 @@ if (is_user_logged_in()) {
     // Redirect to shop main page
     header("location: " . MAIN_PAGE);
     exit();
-} ?>
+}
+
+// Check if password reset was requested
+if (isset($_POST['pwd-reset-submit'])) {
+
+    $mail = filter_input(INPUT_POST, 'inputMail', FILTER_SANITIZE_EMAIL);
+    do_pwd_reset($mail);
+}
+?>
 <!doctype html>
 <html lang="en">
 
@@ -36,21 +44,19 @@ if (is_user_logged_in()) {
 
 </head>
 
-
-
 <body class="text-center">
     <!-- HTML Content BEGIN -->
     <div class="jumbotron shadow bg-light login-card">
-        <form class="form-signin">
+        <form class="form-signin" action="password_reset.php" method="post">
             <h1 class="h3 mb-3 font-weight-normal">Reset Your Password</h1>
 
             <label for="input-mail" class="sr-only">Enter your Mail</label>
-            <input type="email" id="input-mail" class="form-control" aria-describedby="mail-help" placeholder="WWU Mail" required autofocus>
+            <input type="email" name="inputMail" id="input-mail" class="form-control" aria-describedby="mail-help" placeholder="WWU Mail" required autofocus>
             <div class="pb-3" id="info-text">
                 <small>Enter your <strong>@uni-muenster.de</strong> mail address. If you are already registered, you will receive a mail with further instructions to reset your password.</small>
             </div>
 
-            <a href="form_handler.php" id="send-btn" class="btn btn-lg btn-register btn-block">Send Mail</a>
+            <button type="submit" name="pwd-reset-submit" id="pwd-reset-submit" class="btn btn-lg btn-register btn-block">Send Mail</button>
             <a href="index.php" class="btn btn-link login-link">Back to Login Page</a>
 
             <p class="mt-5 mb-3 text-muted">&copy; <?php get_semester() ?></p>
