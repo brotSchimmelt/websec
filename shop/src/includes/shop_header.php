ebc618c27e7b2c40a8a0b8ff7f5331a579c8c206
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-08-09 19:24:28 +0200

    change logo color

--- shop/src/includes/shop_header.php
+++ shop/src/includes/shop_header.php
@@ -4,7 +4,7 @@ $num = get_number_of_cart_items();
 <nav class="site-header sticky-top py-1" id="main-navbar">
     <div class="container d-flex flex-column flex-md-row justify-content-between">
         <a class="py-2d-none d-md-inline-block" href="/shop/main.php" aria-label="Main Page">
-            <img class="mt-1 mb-2" src="/assets/img/wwu_cysec_inverted.png" width="90" height="40">
+            <img class="mt-1 mb-2" src="/assets/img/wwu_cysec.png" width="90" height="40">
         </a>
         <a class="py-2 d-none d-md-inline-block mt-2" href="/shop/main.php">Home</a>
         <a class="py-2 d-none d-md-inline-block mt-2" href="/shop/overview.php">Products</a>
