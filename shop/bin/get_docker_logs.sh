836774fe868b12556b1251483954025f53228c22
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-08-09 19:43:25 +0200

    add mysql logs

--- shop/bin/get_docker_logs.sh
+++ shop/bin/get_docker_logs.sh
@@ -6,9 +6,9 @@ printf "## The relevant commands can also be found under: https://docs.docker.co
 
 # Select log files
 user_input=-1
-while (( $user_input < 1 || $user_input > 4 ))
+while (( $user_input < 1 || $user_input > 5 ))
 do
-printf "\n1) Apache error logs 2) Apache access logs \n3) MSMPT logs 4) All of the above\n\n"
+printf "\n1) Apache error logs 2) Apache access logs \n3) MSMPT (mail) logs 4) MySQL logs \n5) All of the above\n\n"
 printf "Select an option: "
 read user_input
 done
@@ -29,6 +29,11 @@ case $user_input in
         docker cp php_apache:/var/log/msmtp/msmtp.log msmtp.log 2>/dev/null
         ;;
     4)
+        printf "creating file ...\n"
+        docker logs -t db_login 2> mysql_login.log > /dev/null
+        docker logs -t db_shop 2> mysql_shop.log > /dev/null
+        ;;
+    5)
         printf "creating files ...\n"
         docker logs -t --details php_apache > /dev/null 2> errors_php.log
         docker logs -t --details php_apache 2> /dev/null > access.log
