96357329be8f4384065b3c12ae9a764dc808f9e5
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-08-16 20:00:15 +0200

    change js import order

---
+++ shop/src/includes/bootstrap_js.php
@@ -0,0 +1,3 @@
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="../assets/js/vendor/jquery-3.5.1.min.js"></script>
+<script src="../assets/js/vendor/bootstrap.bundle.js"></script>
--- shop/src/includes/shop_js.php
+++
@@ -1,5 +0,0 @@
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="../assets/js/vendor/jquery-3.5.1.min.js"></script>
-<script src="../assets/js/vendor/popper.min.js"></script>
-<script src="../assets/js/vendor/bootstrap.js"></script>
-<script src="../assets/js/vendor/bootstrap.bundle.js"></script>
