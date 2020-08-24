232cd2027f73c3b5dc90af2682f4e5e97d40f78d
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-08-24 22:18:03 +0200

    add XSS solution modal

---
+++ shop/src/messages.php
@@ -0,0 +1,35 @@
+<!--Modal: Login with Avatar Form-->
+<div class="modal fade" id="xss-solution" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog cascading-modal modal-avatar modal-sm" role="document">
+        <!--Content-->
+        <div class="modal-content">
+
+            <!--Header-->
+            <!-- <div class="modal-header">
+                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%281%29.jpg" alt="avatar" class="img-responsive">
+            </div> -->
+            <!--Body-->
+            <div class="modal-body text-center mb-1">
+
+                <h5 class="mt-1 mb-2">Enter Cookie</h5>
+
+                <div class="md-form ml-0 mr-0">
+                    <p>Have you found the XSS session cookie?</p>
+                    <input type="text" id="form29" placeholder="XSS_Your_Session" autofocus>
+                </div>
+
+                <div class="text-center mt-4">
+                    <button class="btn btn-primary">Send</button>
+                </div>
+            </div>
+
+        </div>
+        <!--/.Content-->
+    </div>
+</div>
+<!--Modal: Login with Avatar Form-->
+
+<!-- <div class="text-center">
+    <a href="" class="btn btn-default btn-rounded" data-toggle="modal" data-target="#modalLoginAvatar">Launch
+        Modal Login with Avatar</a>
+</div> -->
