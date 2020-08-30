1f6c95fdccdf15134e8812a4d46d659db6bf1b26
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-08-30 13:54:51 +0200

    add modal javascript

---
+++ shop/src/includes/shop_js.php
@@ -0,0 +1,32 @@
+<?php
+/*
+* Java Script for modals
+*/
+// show modal with field to enter solution
+if (isset($searchFieldWasUsed)) {
+    if ($searchFieldWasUsed && preg_match("/document.cookie/", $rawSearchTerm)) {
+        echo "
+<script>
+    $('#xss-solution').modal('show')
+</script>";
+    }
+}
+
+// show failure modal
+if (isset($challengeFailed)) {
+    if ($challengeFailed) {
+        echo "
+<script>
+    $('#xss-wrong').modal('show')
+</script>";
+    }
+}
+// show success modal
+if (isset($showSuccessModal)) {
+    if ($showSuccessModal) {
+        echo "
+<script>
+    $('#challenge-success').modal('show')
+</script>";
+    }
+}
