a1e84f797ad925de8913d214217e5d7e2a2e553a
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-09-06 23:11:54 +0200

    check difficulty setting

--- shop/src/includes/shop_header.php
+++ shop/src/includes/shop_header.php
@@ -8,10 +8,18 @@ require_once(FUNC_LOGIN);
 require_once(FUNC_BASE);
 
 $num = get_number_of_cart_items();
-$sqliSolved = lookup_challenge_status("sqli", $_SESSION['userName']);
 
-if (!lookup_challenge_status("stored_xss", $_SESSION['userName'])) {
-    compare_cookies($_SESSION['userName']);
+// check SQLi and stored XSS challenge status
+if (get_global_difficulty() == "hard") {
+    $sqliSolved = lookup_challenge_status("sqli_hard", $_SESSION['userName']);
+    if (!lookup_challenge_status("stored_xss_hard", $_SESSION['userName'])) {
+        compare_cookies($_SESSION['userName']);
+    }
+} else {
+    $sqliSolved = lookup_challenge_status("sqli", $_SESSION['userName']);
+    if (!lookup_challenge_status("stored_xss", $_SESSION['userName'])) {
+        compare_cookies($_SESSION['userName']);
+    }
 }
 
 if (isset($_SESSION['fakeCart']) && $_SESSION['fakeCart'] == true) {
