00e92dcf985e7aff331c3d2352362a684c526a6c
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-09-10 14:32:24 +0200

    clean up javascript for admin panel

--- shop/src/includes/admin_js.php
+++ shop/src/includes/admin_js.php
@@ -1,12 +1,5 @@
-<!-- <script src="../assets/js/vendor/jquery-3.5.1.slim.min.js"></script> -->
-<script src="../assets/js/vendor/jquery-3.5.1.min.js"></script>
-<script src="../assets/js/vendor/bootstrap.bundle.min.js"></script>
-<script src="../assets/js/vendor/feather.min.js"></script>
-<script src="../assets/js/vendor/Chart.min.js"></script>
-<script src="../assets/js/dashboard.js"></script>
-
-<!-- Search Script -->
 <script>
+    // Search Script
     $(document).ready(function() {
         $("#user-search").on("keyup", function() {
             var value = $(this).val().toLowerCase();
