7afb9b09d3394f67ed1702c8f7060c0ca6f7a12c
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-09-12 11:43:06 +0200

    fix logo in footer

--- src/includes/shop_footer.php
+++ src/includes/shop_footer.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-12 col-md">
             <a class="py-2d-none d-md-inline-block pb-3" href="/shop/main.php" aria-label="Main Page">
-                <img class="mt-1 mb-2" src="/assets/img/wwu_cysec.png" width="100" height="45">
+                <img class="mt-1 mb-2" src="/assets/img/fake_logo.png" width="128" height="72">
             </a>
             <small class="d-block mb-3 text-muted">&copy; <?php get_semester() ?></small>
         </div>
@@ -34,7 +34,7 @@
             </ul>
         </div>
         <div class="col-6 col-md">
-            <a class="py-2d-none d-md-inline-block pb-3" href="#" aria-label="Main Page">
+            <a class="py-2d-none d-md-inline-block pb-3 ml-5 text-muted" href="" aria-label="Main Page">
                 <svg width="2.5em" height="2.5em" viewBox="0 0 16 16" class="bi bi-arrow-bar-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M11.354 5.854a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L8 3.207l2.646 2.647a.5.5 0 0 0 .708 0z" />
                     <path fill-rule="evenodd" d="M8 10a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-1 0v6.5a.5.5 0 0 0 .5.5zm-4.8 1.6c0-.22.18-.4.4-.4h8.8a.4.4 0 0 1 0 .8H3.6a.4.4 0 0 1-.4-.4z" />
