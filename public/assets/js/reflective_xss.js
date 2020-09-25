cf6b852ef12409a5149f8df64260604a39ba6ec3
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-09-25 12:29:30 +0200

    add support for confirm() and promopt()

--- public/assets/js/reflective_xss.js
+++ public/assets/js/reflective_xss.js
@@ -7,3 +7,24 @@
         $('#xss-solution').modal('show');
     };
 })();
+
+(function () {
+    var _old_prompt = window.prompt;
+    window.prompt = function () {
+        // run some code BEFORE alert
+        _old_prompt.apply(window, arguments);
+        // run some code AFTER alert
+        $('#xss-solution').modal('show');
+    };
+})();
+
+(function () {
+    var _old_confirm = window.confirm;
+    window.confirm = function () {
+        // run some code BEFORE alert
+        _old_confirm.apply(window, arguments);
+        // run some code AFTER alert
+        $('#xss-solution').modal('show');
+    };
+})();
+
