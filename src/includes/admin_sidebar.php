b1de2252fb65a6b8527c059e54a12bc67e9ccd39
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-10-09 15:18:57 +0200

    add link to phpmyadmin

--- src/includes/admin_sidebar.php
+++ src/includes/admin_sidebar.php
@@ -46,7 +46,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="http://localhost:8081" target="_blank">
+                <a class="nav-link" href=<?= "http://" . SITE_URL . ":8081" ?> target="_blank">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-hdd-stack-fill mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M2 9a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-1a2 2 0 0 0-2-2H2zm.5 3a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2 0a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zM2 2a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm.5 3a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2 0a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
                     </svg>
@@ -54,7 +54,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="http://localhost:8080" target="_blank">
+                <a class="nav-link" href=<?= "http://" . SITE_URL . ":8080" ?> target="_blank">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-hdd-stack-fill mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M2 9a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-1a2 2 0 0 0-2-2H2zm.5 3a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2 0a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zM2 2a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm.5 3a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2 0a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
                     </svg>
