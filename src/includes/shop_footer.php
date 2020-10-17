9462e8e804fdeab01c886245dacae8ae6ca90797
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-10-17 10:12:54 +0200

    fix anker position

--- src/includes/shop_footer.php
+++ src/includes/shop_footer.php
@@ -36,8 +36,8 @@
                 <li><a class="text-muted" href="<?= get_setting("learnweb", "link") ?>">Learnweb</a></li>
             </ul>
         </div>
-        <div class="col-6 col-md">
-            <a class="py-2d-none d-md-inline-block pb-3 ml-5 text-muted" href="" aria-label="Main Page">
+        <div class="col-6 col-md d-none d-md-block justify-content-start">
+            <a class="text-muted justify-content-start" href="" aria-label="Main Page">
                 <svg width="2.5em" height="2.5em" viewBox="0 0 16 16" class="bi bi-arrow-bar-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M11.354 5.854a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L8 3.207l2.646 2.647a.5.5 0 0 0 .708 0z" />
                     <path fill-rule="evenodd" d="M8 10a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-1 0v6.5a.5.5 0 0 0 .5.5zm-4.8 1.6c0-.22.18-.4.4-.4h8.8a.4.4 0 0 1 0 .8H3.6a.4.4 0 0 1-.4-.4z" />
