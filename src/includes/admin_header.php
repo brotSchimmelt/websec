2503e2f5b10fa22163b9af2aa66f45ce4544263e
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-10-25 11:18:29 +0100

    fix nav brand

--- src/includes/admin_header.php
+++ src/includes/admin_header.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="dashboard.php">WebSec Admin Dashboard</a>
+    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="dashboard.php">WebSec Admin</a>
 
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
