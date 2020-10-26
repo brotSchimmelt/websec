6bde10ac5040e02ccffe9b373b2b67ed59ae3368
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-10-26 22:16:14 +0100

    open mail form again

--- public/password_reset.php
+++ public/password_reset.php
@@ -56,11 +56,7 @@ if (isset($_POST['pwd-reset-submit'])) {
             <?= get_message(); ?>
 
             <label for="input-mail" class="sr-only">Enter your Mail</label>
-            <input type="email" name="inputMail" id="input-mail" class="form-control" aria-describedby="mail-help" placeholder="WWU Mail" required autofocus disabled>
-            <br>
-            <div class="alert alert-danger shadow" role="alert">
-                The mail system is currently offline.
-            </div>
+            <input type="email" name="inputMail" id="input-mail" class="form-control" aria-describedby="mail-help" placeholder="WWU Mail" required autofocus>
             <div class="pb-3" id="info-text">
                 <small>Enter your <strong>@uni-muenster.de</strong> mail address. If you are already registered, you will receive a mail with further instructions to reset your password.</small>
             </div>
