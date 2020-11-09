bebf9c67a5f1e71a1af5fd19e5f3b89bedc22428
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-11-09 16:48:23 +0100

    add browser support information

--- src/includes/instruction_general.php
+++ src/includes/instruction_general.php
@@ -18,7 +18,7 @@
         This can be done by accessing the challenge settings in the account menu.
     </p>
     <p>External tools: All challenges can (and must) be solved without the use of external tools! We keep track of how you solve the challenges and using any software, e.g., for automation, will make you immediately fail! You are here to learn about web hacking and not about how to run a specific toolchain.</p>
-    <p>Browser Support: This website should work with all modern desktop browsers. Mobile browsers are not currently supported.</p>
+    <p>Browser Support: This website works best with the newest version of Firefox, Chrome or Edge. Safari, Internet Explorer and mobile browsers are currently <b>not</b> supported.</p>
     <br>
 <?php else : ?>
     <!-- Hard -->
