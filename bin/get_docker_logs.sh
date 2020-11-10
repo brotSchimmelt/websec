1d49d2db5a3c7a7b18d232bc91a5a4f76fd3d677
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-11-10 10:36:07 +0100

    fix container names

--- bin/get_docker_logs.sh
+++ bin/get_docker_logs.sh
@@ -10,6 +10,11 @@
 #   Copy log files from docker containers                                      #
 ################################################################################
 
+# get container names
+php_apache="$(docker ps --format "{{.Names}}" | grep php_apache)"
+db_login="$(docker ps --format "{{.Names}}" | grep db_login)"
+db_shop="$(docker ps --format "{{.Names}}" | grep db_shop)"
+
 printf "## This script outputs all relevant log files\n"
 printf "## from the docker container into this directory.\n\n"
 printf "## The relevant commands can also be found under: "\
@@ -24,31 +29,32 @@ printf "Select an option: "
 read user_input
 done
 
-
 # Choose log file option
 case $user_input in
     1) 
         printf "creating file ...\n"
-        docker logs -t --details php_apache > /dev/null 2> errors_php.log
+        docker logs -t --details $php_apache > /dev/null 2> errors_php.log
         ;;
     2)
         printf "creating file ...\n"
-        docker logs -t --details php_apache 2> /dev/null > access.log
+        docker logs -t --details $php_apache 2> /dev/null > access.log
         ;;
     3)
         printf "creating file ...\n"
-        docker cp php_apache:/var/log/msmtp/msmtp.log msmtp.log 2>/dev/null
+        docker cp $php_apache:/var/log/msmtp/msmtp.log msmtp.log 2>/dev/null
         ;;
     4)
         printf "creating file ...\n"
-        docker logs -t db_login 2> mysql_login.log > /dev/null
-        docker logs -t db_shop 2> mysql_shop.log > /dev/null
+        docker logs -t $db_login 2> mysql_login.log > /dev/null
+        docker logs -t $db_shop 2> mysql_shop.log > /dev/null
         ;;
     5)
         printf "creating files ...\n"
-        docker logs -t --details php_apache > /dev/null 2> errors_php.log
-        docker logs -t --details php_apache 2> /dev/null > access.log
-        docker cp php_apache:/var/log/msmtp/msmtp.log msmtp.log 2>/dev/null
+        docker logs -t --details $php_apache > /dev/null 2> errors_php.log
+        docker logs -t --details $php_apache 2> /dev/null > access.log
+        docker cp $php_apache:/var/log/msmtp/msmtp.log msmtp.log 2>/dev/null
+        docker logs -t $db_login 2> mysql_login.log > /dev/null
+        docker logs -t $db_shop 2> mysql_shop.log > /dev/null
         ;;
     *)
         echo "Error: No valid choice"
