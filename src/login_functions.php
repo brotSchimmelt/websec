58398772650950b65be2304ab1d65f8cd83c9a0f
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-11-13 23:45:35 +0100

    hard code variables for test cases

--- src/login_functions.php
+++ src/login_functions.php
@@ -160,7 +160,9 @@ function validate_mail($mail)
     }
 
     // Note: Add other valid domains in settings.json or in the admin panel
-    $validDomains = get_allowed_domains();
+    // $validDomains = get_allowed_domains();
+    // TESTING: Hard code the allowed domains for phpUnit
+    $validDomains = ["@uni-muenster.de", "@wi.uni-muenster.de"];
 
     // check if a valid domain us used
     $needle = mb_strstr($mail, "@");
@@ -219,7 +221,9 @@ function hash_user_pwd($pwd)
 function check_entry_exists($entry, $sql)
 {
     // Fake user array
-    $fakeUsers = get_blocked_usernames();
+    // $fakeUsers = get_blocked_usernames();
+    // TESTING: hard code the array for phpUnit
+    $fakeUsers = ["admin", "elliot", "l337_h4ck3r", "administrator"];
 
     // Get entries from DB
     try {
