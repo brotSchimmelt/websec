aeee68ae220bfa1c7df8da145c7cf567caa7e218
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-11-14 21:07:23 +0100

    mock login dependencies for phpUnit

---
+++ tests/login_mocked_functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function get_global_difficulty()
+{
+    return "normal";
+}
+
+function create_sqli_db($username, $mail)
+{
+}
+
+function slug($str)
+{
+    return $str;
+}
+
+function get_blocked_usernames()
+{
+    return ["admin", "elliot", "l337_h4ck3r", "administrator"];
+}
+
+function get_allowed_domains()
+{
+    return ["@uni-muenster.de", "@wi.uni-muenster.de"];
+}
