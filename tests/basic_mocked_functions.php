d77a011c600fc036240ac0af46649b853aa6884a
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-11-17 23:12:34 +0100

    mock database connection

---
+++ tests/basic_mocked_functions.php
@@ -0,0 +1,22 @@
+<?php
+
+function get_login_db()
+{
+    // ensure only one connection at a time is alive
+    static $dbLogin;
+    if ($dbLogin instanceof PDO) {
+        return $dbLogin;
+    }
+
+    try {
+        $dbLogin = new PDO(DSN_LOGIN, DB_USER_LOGIN, DB_PWD_LOGIN, OPTIONS_LOGIN);
+    } catch (PDOException $e) {
+        $note = "The connection to our database could not be established. "
+            . 'If this error persists, please post it to the '
+            . '<a href="https://www.uni-muenster.de/LearnWeb/learnweb2/" '
+            . ' target="_blank">Learnweb</a> forum.';
+        display_exception_msg(null, "010", $note);
+        exit();
+    }
+    return $dbLogin;
+}
