ce0ac86bd3fc63272af58a84189770b733c426c0
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-11-18 22:29:05 +0100

    add database connections

---
+++ tests/admin_mocked_funtions.php
@@ -0,0 +1,43 @@
+<?php
+
+function get_login_db()
+{
+    // ensure only one connection at a time is alive
+    static $dbLogin;
+    if ($dbLogin instanceof PDO) {
+        return $dbLogin;
+    }
+
+    try {
+        $dbLogin = new PDO(DSN_LOGIN, DB_USER_LOGIN, DB_PWD_LOGIN, OPTIONS_LOGIN);
+    } catch (PDOException $e) {
+        $note = "The connection to our database could not be established. "
+            . 'If this error persists, please post it to the '
+            . '<a href="https://www.uni-muenster.de/LearnWeb/learnweb2/" '
+            . ' target="_blank">Learnweb</a> forum.';
+        display_exception_msg(null, "010", $note);
+        exit();
+    }
+    return $dbLogin;
+}
+
+function get_shop_db()
+{
+    static $dbShop;
+
+    if ($dbShop instanceof PDO) {
+        return $dbShop;
+    }
+    require_once(CONF_DB_SHOP);
+    try {
+        $dbShop = new PDO(DSN_SHOP, DB_USER_SHOP, DB_PWD_SHOP, OPTIONS_SHOP);
+    } catch (PDOException $e) {
+        $note = "The connection to our database could not be established. "
+            . 'If this error persists, please post it to the '
+            . '<a href="https://www.uni-muenster.de/LearnWeb/learnweb2/" '
+            . 'target="_blank">Learnweb</a> forum.';
+        display_exception_msg(null, "020", $note);
+        exit();
+    }
+    return $dbShop;
+}
