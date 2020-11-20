f67c30e0b0479a5fd688d1ee43d73f90e15fbaae
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-11-20 13:26:11 +0100

    add timer

--- tests/run_all_test_classes.sh
+++ tests/run_all_test_classes.sh
@@ -16,11 +16,14 @@ green="\033[0;32m"
 orange="\033[0;33m"
 noColor="\033[0m"
 
+# set timer to 0
+SECONDS=0
+
 # run all the different test classes sequentially
-for i in Admin Basic Error; do
+for i in Admin Basic Error Login; do
 
     printf "\nTesting ${orange}${i}${noColor} functions ...\n\n"
     $phpunit $test_dir${i}FunctionsTest.php
 done
 
-printf "\n${green}All tests have been run!${noColor}\n"
+printf "\n${green}It took $SECONDS seconds to run all tests!${noColor}\n"
